7ac4a1aaac6f72e7fa1d9845a6848ad6bca16cc2
Falko Krause <falko.krause@staff.hu-berlin.de>
2013-03-07 20:18:51 +0100

    renamed everything to jsBeautify now only executing on Java Script and JSON

--- js_formatter.py
+++ js_formatter.py
@@ -1,39 +1,21 @@
 import sublime
 import sublime_plugin
 import jsbeautifier
-import sys
 
-__version__      = '1.0'
-__core_version__ = '1.0'
-__authors__      = ['"Falko Krause" <falko.krause@gmail.com>']
+# I made this: Falko Krause" <falko.krause@gmail.com>
 
 # This plugin is based on
 # https://github.com/bjouhier/sublime-text-jsbeautifier
 # https://github.com/einars/js-beautify
 
-is_python3 = sys.version_info[0] > 2
-# Settings
-settings = None
 
-
-def init():
-    "Init plugin"
-    # load settings
-    # globals()['user_settings'] = sublime.load_settings('Preferences.sublime-settings')
-    globals()['settings'] = sublime.load_settings('JsFormat.sublime-settings')
+# base_name = sublime.platform() == 'windows' and 'Jsbeautify (Windows).sublime-settings' or 'AutoPep8.sublime-settings'
+base_name = 'JsBeautify.sublime-settings'
 
 
 class JsbeautifyCommand(sublime_plugin.TextCommand):
-    # FIME not sure if I need to parse user settings
-    # def __init__(self, settings=None):
-    # FIXME need to init base class here
-    #     if settings is None:
-    #         # settings = {}
-    #         for k in ['indent_char', 'indent_size', 'max_preserve_newlines']:
-    #             if user_settings.has(k):
-    #                 settings[k] = user_settings.get(k, None)
-
     def run(self, edit):
+        settings = sublime.load_settings(base_name)
         opts = jsbeautifier.default_options()
         opts.indent_char = settings.get('indent_char', None)
         opts.indent_size = settings.get('indent_size', None)
@@ -45,8 +27,5 @@ class JsbeautifyCommand(sublime_plugin.TextCommand):
         self.view.replace(edit, replaceRegion, res)
         self.view.show_at_center(prePos.begin())
 
-
-##################
-# Init plugin
-if not is_python3:
-    init()
+    def is_visible(self, *args):
+        return self.view.settings().get('syntax') == "Packages/JavaScript/JavaScript.tmLanguage" or self.view.settings().get('syntax') == "Packages/JavaScript/JSON.tmLanguage"
